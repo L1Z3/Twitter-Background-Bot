b5c2b67cbe6d6f802d75794920ddec7ff94a1505
Unknown <wcjones12@gmail.com>
2018-07-25 15:38:12 -0400

    Fix error when files are deleted mid-cycle
    Fix "tweepy.error.TweepError: Unable to access file: The system cannot find the file specified" by having the file deleted from the list of files, preventing the program from getting stuck in an infinite loop if a file is deleted.

--- TBackground.py
+++ TBackground.py
@@ -41,24 +41,37 @@ while True:
             continue
     shuffle(name_list)
     i = 0
-    for file in name_list:
-        i += 1
+    removed_file = None
+    while i < len(name_list):
+        if removed_file is not None:
+            name_list.remove(removed_file)
+            removed_file = None
+        break_and_continue = False
         while True:
             try:
-                api.update_profile_banner(file)
+                api.update_profile_banner(name_list[i])
             except tweepy.error.RateLimitError:
                 print "Rate limited exceeded. Trying again in ten minutes."
                 sleep(600)
                 continue
             except tweepy.error.TweepError as e:
-                print "Something went wrong when uploading banner photo. Trying again in 10 seconds."
-                print e
-                sleep(10)
-                continue
+                if "The system cannot find the file specified" in str(e):
+                    print "The photo with name \"" + str(name_list[i]).split("/")[-1] + "\" has been deleted. Removing from list.\n"
+                    removed_file = name_list[i]
+                    break_and_continue = True
+                    break
+                else:
+                    print "Something went wrong when uploading banner photo:"
+                    print e
+                    print "Trying again in 10 seconds.\n"
+                    sleep(10)
+                    continue
             break
-
-        print "Finished uploading pic number " + str(i) + "/" + str(len(name_list)) + " at " + strftime("%I:%M:%S %p on %m/%d.", localtime())
+        if break_and_continue:
+            continue
+        print "Finished uploading pic number " + str(i+1) + "/" + str(len(name_list)) + " at " + strftime("%I:%M:%S %p on %m/%d.", localtime())
         print "It has a filename of \"" + str(name_list[i]).split("/")[-1] + "\". Going again in 15 seconds.\n"
+        i += 1
         sleep(15)
 
     print "Restarting cycle.\n\n\n"
